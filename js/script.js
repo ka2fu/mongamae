fc3b33842eccb7278e6df0161eb974749677bde1
ka2fu <luxury_roppongi@yahoo.co.jp>
2021-01-13 23:14:16 +0900

    changed nav

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@
 
 var headerNavHeight = $('#header-nav').height();
 var headerHeight = $('header').height();
-var spyNavHeight = $('#spy-nav').height();
-var spyStart = $('#spy-nav').offset().top;
+var spyNavHeight = $('#spy-nav-bottom').height();
+var spyTopHeight = $('#spy-nav-top').height();
+var spyStart = $('#spy-nav-bottom').offset().top; // 画面左端からのナビゲーション バーの座標を取得
+var spyBttom = spyStart + spyNavHeight;
 var distance = 0;
 var monColor = 'rgb(194, 0, 0)';
+
 
 $(function(){
 
@@ -80,41 +83,12 @@ $(function(){
   });
 
   // ずれ問題
-  var a_height = headerNavHeight + spyNavHeight*3;
+  var a_height = headerNavHeight + spyNavHeight*2;
   $('a.nav-link').click(function (){
     var href = $(this).attr('href');
     var target = $(href == "#" || href == "" ? "body" : href);
     var position = target.offset().top - a_height;
     $('html, body').animate({ scrollTop: position }, 500, "swing");
     return false;
-  });
-
-  $(document).scroll(function(){
-    distance = $(this).scrollTop() + headerNavHeight;
-
-    // fix navbar
-    if(spyStart <= distance){ // under position
-      $('#spy-nav').addClass('fixed fixed-spy').css('top', headerNavHeight);
-      $('body').attr('data-offset', headerNavHeight+spyNavHeight);
-    }
-    else if(spyStart >= distance){ // over position
-      $('#spy-nav').removeClass('fixed fixed-spy').css('top', '0');
-      $('body').attr('data-offset', distance+spyNavHeight);
-      // $('#header-nav').removeClass('opa');
-    }
-
-    // hover
-    $('#spy-nav').find('li').hover(
-      function (){
-        if($(this).hasClass('spy-active')){
-          $(this).css('filter', 'brightness(1.0)');
-        } else {
-          $(this).css('filter', 'brightness(0.75)');
-        }
-      },
-      function (){
-        $(this).css('filter', 'brightness(1.0)');
-      }
-    );
   });
 });
---
+++ js/spynav.js
@@ -0,0 +1,53 @@
+
+// spy-nvaに関してのスクリプト
+// index.htmlだけに適応したい
+
+var headerNavHeight = $('#header-nav').height();
+var headerHeight = $('header').height();
+var spyNavHeight = $('#spy-nav-bottom').height();
+var spyStart = $('#spy-nav-bottom').offset().top; // 画面左端からのナビゲーション バーの座標を取得
+var spyBttom = spyStart + spyNavHeight;
+var distance = 0;
+var monColor = 'rgb(194, 0, 0)';
+
+
+$(document).scroll(function(){
+  distance = $(this).scrollTop() + headerNavHeight;
+
+  // fix navbar
+  if(spyBttom <= distance){ // under position
+    // 使わない
+    // $('.spy-nav').addClass('fixed fixed-spy').css('top', headerNavHeight);
+    // 使わない
+    $('body').attr('data-offset', headerNavHeight+spyNavHeight);
+    $.when(
+      $('.tori-url').fadeOut(),
+      $('#spy-nav-bottom').find('.nav-link').fadeOut()
+    ).done(function(){
+      $('#spy-nav-top').fadeIn();
+    });
+  }
+  else if(spyBttom > distance){ // over position
+    // 使わない
+    // $('.spy-nav').removeClass('fixed fixed-spy').css('top', '0');
+    // 使わない
+    $('body').attr('data-offset', distance+spyNavHeight);
+    $.when(
+      $('#spy-nav-top').fadeOut()
+    ).done(function(){
+      $('.tori-url').fadeIn();
+      $('#spy-nav-bottom').find('.nav-link').fadeIn();
+    });
+  }
+
+  // hover
+  $('.spy-nav').find('.nav-link').hover(
+    function(){
+      $(this).css('filter', 'brightness(0.85)');
+    },
+    function(){
+      $(this).css('filter', 'brightness(1.0)');
+    }
+  );
+
+});
