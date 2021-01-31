16db61ec1ea4ef9182eec8c90f60471ed3ad7156
ka2fu <luxury_roppongi@yahoo.co.jp>
2021-02-01 04:11:26 +0900

    restart iki

---
+++ js/ikidukai.js
@@ -0,0 +1,61 @@
+
+$('#wrapper').on('click', '.title', function(){
+  $.when(
+    $('.title-inner').fadeOut()
+  ).done(function(){
+    $('#ppl-inner').removeClass('vanished');
+    $('#ppl-inner').fadeIn();
+  });
+});
+
+
+// modalの開閉
+$('nav').on('click', '#open', function (){
+  if($('#modal').hasClass('hidden')){ // modalの顔一瞬出てくる現象抑制　
+    $('#modal').hide();
+    $('#modal').removeClass('hidden');
+  }
+
+  $('#open').fadeOut();
+  $('#header-wrapper').addClass('border-inherit');
+  $('#modal').slideDown();
+
+  // modalを開く時背景色を変更
+  $('#header-wrapper').animate({
+    backgroundColor: "rgba(255, 255, 255, 0.65)"
+  }, {
+    duration: 600,
+    easing: "easeOutCubic",
+    queue: false
+  });
+  $('#modal').animate({
+    backgroundColor: "rgba(255, 255, 255, 0.65)"
+  }, {
+    duration: 600,
+    easing: "easeOutCubic",
+    queue: false
+  });
+});
+$('#modal').on('click', '#close', function (){
+
+  // modalを閉じる時背景力を戻す
+  $('#header-wrapper').animate({
+    backgroundColor: "transparent"
+  }, {
+    duration: 600,
+    easing: "easeInCubic",
+    queue: false
+  });
+  $('#modal').animate({
+    backgroundColor: "transparent"
+  }, {
+    duration: 600,
+    easing: "easeInCubic",
+    queue: false
+  });
+
+  $('#modal').slideUp(function (){
+    $('#header-wrapper').removeClass('border-inherit');
+    $('#open').fadeIn();
+  })
+})
