64bdd735e96d7474aa2905a03eb7b3d220387156
ka2fu <luxury_roppongi@yahoo.co.jp>
2021-02-01 00:03:04 +0900

    sp chousei

--- js/spynav.js
+++ js/spynav.js
@@ -24,22 +24,24 @@ $(document).scroll(function(){
   distance = $(this).scrollTop() + headerNavHeight;
 
   // fix navbar
-  if(spyBttom <= distance){ // under position
-    $('body').attr('data-offset', headerNavHeight+spyNavHeight);
-    $.when(
-      $('#header-nav').find('.tori-url').fadeOut(),
-      $('#spy-nav-bottom').find('.nav-link').fadeOut()
-    ).done(function(){
-      $('#spy-nav-top').fadeIn();
-    });
-  }
-  else if(spyBttom > distance){ // over position
-    $('body').attr('data-offset', distance+spyNavHeight);
-    $.when(
-      $('#spy-nav-top').fadeOut()
-    ).done(function(){
-      $('#header-nav').find('.tori-url').fadeIn();
-      $('#spy-nav-bottom').find('.nav-link').fadeIn();
-    });
+  if(isSmartPhone()){
+    if(spyBttom <= distance){ // under position
+      $('body').attr('data-offset', headerNavHeight+spyNavHeight);
+      $.when(
+        $('#header-nav').find('.tori-url').fadeOut(),
+        $('#spy-nav-bottom').find('.nav-link').fadeOut()
+      ).done(function(){
+        $('#spy-nav-top').fadeIn();
+      });
+    }
+    else if(spyBttom > distance){ // over position
+      $('body').attr('data-offset', distance+spyNavHeight);
+      $.when(
+        $('#spy-nav-top').fadeOut()
+      ).done(function(){
+        $('#header-nav').find('.tori-url').fadeIn();
+        $('#spy-nav-bottom').find('.nav-link').fadeIn();
+      });
+    }
   }
 });
